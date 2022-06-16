15b3a430be14148b0e33c05d444bf557d67097cc
Yu Jia <rockjia1996@gmail.com>
2022-06-15 20:34:59 -0400

    Add test cases on StringType().lowercase(), StringType().uppercase().

--- tests/test_string_type_validations.py
+++ tests/test_string_type_validations.py
@@ -28,3 +28,32 @@ class TestStringTypeValidations(unittest.TestCase):
         
         self.assertIsInstance(errors["test1"][0], ValidationError)
         self.assertIsInstance(errors["test2"][0], ValidationError)
+
+
+    def test_lowercase_uppercase(self):
+        schema = {
+            'test1': StringType().min(3).max(64).lowercase(),
+            'test2': StringType().min(3).max(64).uppercase(),
+            'test3': StringType().lowercase().email()
+        }
+
+        matched_data = {
+            "test1": "every character is in lowercase",
+            "test2": "ALL CHARACTERS ARE IN UPPERCASE",
+            "test3": "username@domain.com"
+        }
+
+        unmatched_data = {
+            "test1": "fail TEST",
+            "test2": "fail test",
+            "test3": "USER@DOMAIN.COM"
+        }
+
+        empty_errors = Validator.validate(schema, matched_data)
+        errors = Validator.validate(schema, unmatched_data)
+
+        self.assertEqual(empty_errors, {})
+
+        self.assertEqual(errors["test1"][0].get_error_tag(), "LOWERCASE_ERROR")
+        self.assertEqual(errors["test2"][0].get_error_tag(), "UPPERCASE_ERROR")
+        self.assertEqual(errors["test1"][0].get_error_tag(), "LOWERCASE_ERROR")
